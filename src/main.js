145a6bbe0054c04c390fd19a09857613c951543e
luisdlpr <reyesluichi@hotmail.com>
2026-08-10 21:20:34 +1000

    feat: add avatar + logo and layout tweaking

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
 import './style.css'
+import avatar from './assets/avatar.webp'
+import banner from './assets/carbon.png'
+
+document.getElementById('avatar').src = avatar
+document.getElementById('banner').src = banner
 
